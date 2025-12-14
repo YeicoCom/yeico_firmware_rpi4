e70a1d3aaed4d61f6522e3c89017029d18b3a3dc
Samuel Ventura <samuel.ventura@yeico.com>
2025-12-13 22:30:47 -0600

    WIP

---
+++ shell.sh
@@ -0,0 +1,5 @@
+
+~/yeico_firmware_rpi4/deps/nerves_system_br/create-build.sh \
+    ~/yeico_firmware_rpi4/nerves_defconfig \
+    ~/yeico_firmware_rpi4/.nerves/artifacts/yeico_firmware_rpi4-portable-1.32.0 >/dev/null && \
+    cd ~/yeico_firmware_rpi4/.nerves/artifacts/yeico_firmware_rpi4-portable-1.32.0
